bf42773834bf14e620f1d77fd610e3b536433b97
FallingSn0w <90278570+FallingSn0w@users.noreply.github.com>
2022-03-25 11:08:36 -0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,17 +1,13 @@
-import java.awt.image.PackedColorModel;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Scanner;
-import java.util.logging.SimpleFormatter;
+import java.util.ArrayList;
+import java.util.Date;
 
-public class Customer {
+public class Customer{
     private int accountNumber;
     static ArrayList<Deposit> deposits;
     static ArrayList<Withdraw> withdraws;
     public static double checkBalance;
     public static double savingBalance;
-    private double savingRate;
     private static String name;
     public static final String CHECKING = "CHECKING";
     public static final String SAVING = "SAVING";
@@ -23,59 +19,65 @@ public class Customer {
         this.withdraws = new ArrayList<Withdraw>();
         this.checkBalance = 0;
         this.savingBalance = 0;
-        this.savingRate = 0;
         this.name = "";
     }
-    Customer(String name, int accountNumber){
+
+    Customer(String name, int accountNumber, double checkBalance, double savingBalance){
         this.name = name;
         this.accountNumber = accountNumber;
         this.checkBalance = checkBalance;
         this.savingBalance = savingBalance;
     }
 
-    public String getName() {
-        System.out.println("Please enter a name: ");
-        Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        this.name = input;
-        return name;
+public String getName(){
+    System.out.println("Please enter a name: ");
+    Scanner scan = new Scanner(System.in);
+    String input = scan.nextLine();
+    this.name = input;
+    return name;
     }
-    public static String setName(String name){return name;}
-    public int getAccountNumber(){
-        accountNumber++;
-        return accountNumber;
-    }
+    public static String setName(String name){
+        return name;}
+
     public static double getCheckBalance(){
         checkBalance = 0;
         for(double i:Main.checking)
             checkBalance+=i;
+        return checkBalance;
+    }
+    public static Double setCheckBalance(double checkBalance){
         System.out.print("Checking: $"+checkBalance);
         return checkBalance;
     }
+
     public static double getSavingBalance(){
         savingBalance = 0;
         for(double i:Main.savings)
             savingBalance+=i;
-        System.out.print("Savings: $"+savingBalance);
         return savingBalance;
     }
     public static Double setSavingBalance(double savingBalance){
+        System.out.print("Savings: $"+savingBalance);
         return savingBalance;
     }
-    public static Double setCheckingBalance(double checkBalance){
-        return checkBalance;
-    }
-    public static String getCustomerProfile(double checkBalance,double savingBalance){
-        System.out.println("Name: "+setName(name)+"   Savings: $"+savingBalance+"   Checking: $"+checkBalance);
+
+    public static String getCustomerProfile(){
+        System.out.println("Name: "+setName(name)+"   Savings: $"+getSavingBalance()+"   Checking: $"+getCheckBalance());
         return "";
     }
 
+    public int getAccountNumber(){
+        accountNumber++;
+        return accountNumber;
+    }
+
     public static void deposit(){
-        Deposit aDeposit = new Deposit(0,new Date(),"");
-        Deposit newDeposit = new Deposit(aDeposit.getAmount(),aDeposit.getDate(),aDeposit.getAccount(Main.checking, Main.savings));
+        Deposit aDeposit = new Deposit(0,"",new Date());
+        Deposit newDeposit = new Deposit(aDeposit.getAmount(),aDeposit.getAccount(Main.checking, Main.savings),aDeposit.getDate());
         newDeposit.toString();
         deposits.add(newDeposit);
     }
+
     public static void withdraw(){
         Withdraw aWithdraw = new Withdraw(0,new Date(), "");
         Withdraw newWithdraw = new Withdraw(aWithdraw.getAmount(),aWithdraw.getDate(), aWithdraw.getAccount(Main.checking,Main.savings));
@@ -83,11 +85,9 @@ public class Customer {
         withdraws.add(newWithdraw);
     }
 
-
     static boolean checkOverdraft(double amt, String account){
         if(amt>OVERDRAFT){return true;}else{return false;}
     }
-
 
     public static void displayDeposits(){for(Deposit d : deposits){System.out.println(d);}}
     public static void displayWithdraws(){for(Withdraw w : withdraws){System.out.println(w);}}
